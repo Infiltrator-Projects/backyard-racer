89d701fff72c2c8725710f356f2bc8d9c7d877f7
Shannon Smith <infiltratr@yandex.com>
2026-09-12 21:10:43 +1000

    Fix framebuffer include structure

--- src/main_framebuffer_pass.cpp
+++ src/main_framebuffer_pass.cpp
@@ -3,5 +3,4 @@
 // cutover can be reviewed independently from the generated prototype assets.
 #include "main_framebuffer_part1.inc"
 #include "main_framebuffer_part2.inc"
-#include "main_framebuffer_helpers.inc"
 #include "main_framebuffer_part3.inc"
