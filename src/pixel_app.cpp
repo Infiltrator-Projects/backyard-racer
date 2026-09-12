ede25c10950bb6c900432faf09b1487befcc5ddd
Shannon Smith <infiltratr@yandex.com>
2026-09-12 22:54:55 +1000

    Restore pixel renderer include order

--- src/pixel_app.cpp
+++ src/pixel_app.cpp
@@ -1,16 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Pixel renderer kept split while the architecture cutover lands.
 #include "pixel_app_part1.inc"
-
-} // namespace
-} // namespace backyard_racer
-
-namespace backyard_racer::scene_assets {
-#include "mustang_hd_asset.inc"
-}
-
-namespace backyard_racer {
-namespace {
 #include "pixel_app_part2.inc"
 #include "pixel_app_part3.inc"
 #include "pixel_app_part4.inc"
