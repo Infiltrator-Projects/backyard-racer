54b268141074ecd51f98d9c09a7e087a7c629f58
Shannon Smith <infiltratr@yandex.com>
2026-09-12 21:49:35 +1000

    Restore project metadata in pixel entry point

--- src/main_pixel.cpp
+++ src/main_pixel.cpp
@@ -4,10 +4,24 @@
 
 #include <infiltratr/core.h>
 
-#include "generated_assets.h"
-
 #include <exception>
 #include <iostream>
+
+namespace backyard_racer {
+
+const InfiltratrProjectInfo& project_info() {
+    static const InfiltratrProjectInfo info = {
+        sizeof(InfiltratrProjectInfo), INFILTRATR_PROJECT_INFO_ABI,
+        "Backyard Racer", "backyard-racer", "au.com.infiltrator.backyard-racer",
+        "0.6.0-dev", "Infiltrator-Projects/backyard-racer", "development",
+        "Shannon Smith", "https://github.com/Infiltrator-Projects/backyard-racer",
+        "GPL-3.0-or-later", "Street-rod garage and racing game", "backyard-racer",
+        "Copyright (c) 2026 Shannon Smith"
+    };
+    return info;
+}
+
+} // namespace backyard_racer
 
 int main(int argc, char** argv) {
     const InfiltratrProjectInfo& info = backyard_racer::project_info();
