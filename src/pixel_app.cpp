b29cba340809456ff5a7acc55409507b0c90f076
Shannon Smith <infiltratr@yandex.com>
2026-09-12 23:37:29 +1000

    Smooth scene scaling and use HD car art

--- src/pixel_app.cpp
+++ src/pixel_app.cpp
@@ -8,7 +8,10 @@ inline constexpr const IndexedAsset& garage_hd = garage;
 }
 
 #include "pixel_app_part1.inc"
+#include "pixel_bilinear_scaling.inc"
+#define scaled_copy scaled_copy_bilinear
 #include "pixel_app_part2.inc"
 #include "pixel_app_part3.inc"
 #include "pixel_app_part4.inc"
 #include "pixel_app_part5.inc"
+#undef scaled_copy
