8fab1e4c8e9a289c688efac3832aa0198838e05c
Shannon Smith <infiltratr@yandex.com>
2026-09-12 22:56:37 +1000

    Register higher-fidelity Mustang asset

--- src/pixel_app.cpp
+++ src/pixel_app.cpp
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // Pixel renderer kept split while the architecture cutover lands.
+#include "scene_embedded_assets.h"
+namespace backyard_racer::scene_assets {
+#include "mustang_hd_asset.inc"
+}
+
 #include "pixel_app_part1.inc"
 #include "pixel_app_part2.inc"
 #include "pixel_app_part3.inc"
