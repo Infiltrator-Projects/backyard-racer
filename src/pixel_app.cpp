fb375eee4d4e6f9226aee84846046d8ac05def82
Shannon Smith <infiltratr@yandex.com>
2026-09-19 23:58:28 +1000

    Use complete vehicle model art roster

--- src/pixel_app.cpp
+++ src/pixel_app.cpp
@@ -8,8 +8,7 @@
 
 namespace backyard_racer::scene_assets {
 #include "garage_hd_asset.inc"
-#include "falcon_hd_asset.inc"
-#include "mustang_hd_asset.inc"
+#include "vehicle_model_assets.inc"
 #include "ui_font_asset.inc"
 #include "menu_chrome_asset.inc"
 }
